6232a272f8b2849ff2ca6ed6153b8e5a8ae933de
HJJ <ehwja2@gmail.com>
2024-07-18 00:16:20 +0900

    Link 태크 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import MainPageComponent from "./main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPageComponent />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,7 @@
+import React from "react";
 import "./index.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function MainPage() {
   axios
@@ -15,27 +17,37 @@ function MainPage() {
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="images/icons/logo.png" />
+          <img src="images/icons/logo.png" alt="asd" />
         </div>
       </div>
       <div id="body">
         <div id="banner">
-          <img src="images/banners/banner1.png" />
+          <img src="images/banners/banner1.png" alt="" />
         </div>
         <h1>판매되는 상품들</h1>
         <div id="product-list"></div>
         <div className="product-card">
-          <div>
-            <img className="product-img" src="images/products/keyboard1.jpg" />
-          </div>
-          <div className="product-contents">
-            <span className="product-name">키보드</span>
-            <span className="product-price">50000원</span>
-            <div className="product-seller">
-              <img className="product-avatar" src="images/icons/avatar.png" />
-              <span>그랩</span>
+          <Link className="product-link" to={`/products/1`}>
+            <div>
+              <img
+                className="product-img"
+                src="images/products/keyboard1.jpg"
+                alt=""
+              />
             </div>
-          </div>
+            <div className="product-contents">
+              <span className="product-name">키보드</span>
+              <span className="product-price">50000원</span>
+              <div className="product-seller">
+                <img
+                  className="product-avatar"
+                  src="images/icons/avatar.png"
+                  alt=""
+                />
+                <span>그랩</span>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
 
